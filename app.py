6bd7a0da4d6106cf7c8d4c9c792cee6178a482ed
Javeria <j.f.rumana@gmail.com>
2025-11-08 19:49:42 +0530

    Updated to deploy without errors

--- app.py
+++ app.py
@@ -32,4 +32,6 @@ def home():
     return render_template("index.html", weather=weather_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
